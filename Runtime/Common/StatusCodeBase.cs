aa7717ab3f2c33b5b7d7de92d87b49708f954285
Yevhen Bondarenko <yevhen.bondarenko@taltech.ee>
2022-10-05 00:12:19 +0300

    fix: Make StatusCode's value an enum by default

--- Runtime/Common/StatusCodeBase.cs
+++ Runtime/Common/StatusCodeBase.cs
@@ -11,18 +11,18 @@ namespace EE.TalTech.IVAR.Robotics.ROSIndustrial.Common
         /// <summary>
         /// Integer status code.
         /// </summary>
-        public readonly int code;
+        public readonly TCodeEnum value;
         /// <summary>
         /// Name of the status code.
         /// </summary>
-        public readonly string codeName;
+        public readonly string name;
 
-        public StatusCodeBase(int code)
+        public StatusCodeBase(int value)
         {
-            this.code = code;
-            codeName = Enum.GetName(typeof(TCodeEnum), code);
+            this.value = (TCodeEnum)(object)value;
+            name = Enum.GetName(typeof(TCodeEnum), value);
         }
 
-        public override string ToString() { return codeName; }
+        public override string ToString() { return name; }
     }
 }
